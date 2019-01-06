ca7ae9afa57b09ca09867a6748b449041d483ab8
Anton Rudacov <anton.rudacov@gmail.com>
2019-01-06 15:50:03 +0100

    ECLAT WORKS

--- src/main/java/de/tuberlin/campus/dwbi/jobs/ECLATJob.java
+++ src/main/java/de/tuberlin/campus/dwbi/jobs/ECLATJob.java
@@ -8,33 +8,39 @@ import de.tuberlin.campus.dwbi.functions.keyselectors.ItemSetKeySelector;
 import de.tuberlin.campus.dwbi.functions.mappers.ProductIdMapper;
 import de.tuberlin.campus.dwbi.functions.mappers.CountSupportMapper;
 import de.tuberlin.campus.dwbi.functions.reducers.TransactionsProductsReducer;
+import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.core.fs.FileSystem;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.SortedSet;
 
 public class ECLATJob {
 
-    private final static String CSV_FILE = "./src/main/resources/online_retail.csv";
-    //private final static String CSV_FILE = "./src/main/resources/OnlineRetail_short.csv";
+    private final static String INPUT_FILE = "./src/main/resources/online_retail.csv";
+    private final static String OUTPUT_FILE = "./src/main/resources/output.txt";
+    private final static int MIN_SUPPORT = 300;
 
     public static void main(String[] args) throws Exception {
+
+        // Track runtime
         long startTime = System.currentTimeMillis();
 
-        //final int MIN_SUPPORT = (int) (Files.lines(Paths.get(CSV_FILE)).count() * 0.001);
-        final int MIN_SUPPORT = 200;
+        // Print info for ongoing execution
+        printStartupInfo();
 
-        System.out.println("Starting execution with MIN_SUPPORT: " + MIN_SUPPORT);
-
+        // Create Flink execution environment
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
         // Extract 'InvoiceNo' and 'StockCode' from CSV data
         // and map it to: <(String) StockCode -> (SortedSet) [InvoiceNo, InvoiceNo, ...]>
         DataSet<Tuple2<String, SortedSet<String>>> productsTransactions = env
-                .readCsvFile(CSV_FILE)
+                .readCsvFile(INPUT_FILE)
                 .fieldDelimiter(",")
                 .includeFields("11000000")
                 .ignoreFirstLine()
@@ -44,18 +50,20 @@ public class ECLATJob {
 
         // Map product identifier (String) to an id (int) for better performance
         // Note: setParallelism(1) is necessary due to incremental product id!
-        DataSet<Tuple2<Integer, SortedSet<String>>> idsTransactions = productsTransactions
+        DataSet<Tuple2<Integer, SortedSet<String>>> singles = productsTransactions
                 .map(new ProductIdMapper())
                 .setParallelism(1)
                 .filter(new SupportFilter<>(MIN_SUPPORT));
 
-        DataSet<Tuple2<SortedSet<Integer>, SortedSet<String>>> pairs = idsTransactions
-                .cross(idsTransactions)
+        // Find frequent pairs
+        DataSet<Tuple2<SortedSet<Integer>, SortedSet<String>>> pairs = singles
+                .cross(singles)
                 .with(new PairsCross())
                 .filter(new SizeFilter(2))
                 .distinct(new ItemSetKeySelector())
                 .filter(new SupportFilter<>(MIN_SUPPORT));
 
+        // Find frequent sets of three items and count total number of occurrence
         DataSet<Tuple3<SortedSet<Integer>, Integer, SortedSet<String>>> triples = pairs
                 .cross(pairs)
                 .with(new TriplesCross())
@@ -64,18 +72,28 @@ public class ECLATJob {
                 .filter(new SupportFilter<>(MIN_SUPPORT))
                 .map(new CountSupportMapper());
 
-        triples.print();
+        // Save results to file
+        triples.sortPartition(1, Order.DESCENDING)
+                .writeAsText(OUTPUT_FILE, FileSystem.WriteMode.OVERWRITE);
 
-        /* Execute program with sink to file
-        itemsQuantity.sortPartition(2, Order.DESCENDING)
-                .setParallelism(1)
-                .writeAsCsv("./src/main/resources/quantity.csv", FileSystem.WriteMode.OVERWRITE);
+        // Execute Flink job
+        env.execute("Flink ECLAT Job");
 
-        env.execute("Flink Batch Job");
-        */
-
+        // Calculate and print runtime in seconds
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
         System.out.println("\nTook time: " + (double) totalTime / 1000 + " s");
+
+        // Print the number of frequent item sets
+        System.out.println("Frequent item sets found: " + Files.lines(Paths.get(OUTPUT_FILE)).count());
+    }
+
+    private static void printStartupInfo() {
+
+        // Print info for ongoing execution
+        System.out.println("Flink ECLAT Job started with following config: ");
+        System.out.println("  INPUT: " + INPUT_FILE);
+        System.out.println("  OUTPUT: " + OUTPUT_FILE);
+        System.out.println("  MIN_SUPPORT: " + MIN_SUPPORT);
     }
 }
